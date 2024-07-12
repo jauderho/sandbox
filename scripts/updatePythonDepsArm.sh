3183815d05045d4d895b9361ae6be07ac38fa0f5
Jauder Ho <jauderho@users.noreply.github.com>
2024-07-12 20:07:24 +0000

    Test mlx dependency updates
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

---
+++ scripts/updatePythonDepsArm.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# Script to check and update requirements for Python apps
+#
+set -euo pipefail
+IFS=$'\n\t'
+
+APP=(
+  "mlx" \
+)
+
+#APP=(
+#  "pshtt" \
+#  "pwnagotchi" \
+#)
+
+# setup git
+git config --local user.name "Jauder Ho Bot"
+git config --local user.email "jauderho-bot@users.noreply.github.com"
+git config --local pull.rebase false
+
+# setup pipenv and python
+#PATH="$HOME/.local/bin:$PATH"
+#pipenv install --python 3.9
+#pipenv shell
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv python3.12
+python3.12 -m pip install --no-cache-dir --upgrade pip --break-system-packages
+python3.12 -m pip install --no-cache-dir --upgrade --user pipenv certifi wheel setuptools packaging --break-system-packages
+#python3.11 -m pip install --no-cache-dir --upgrade --user pipenv
+#python3.11 -m pip install --no-cache-dir --upgrade --user certifi
+#DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv 
+#python -m pip install --no-cache-dir --upgrade pip
+#python -m pip install --no-cache-dir --upgrade --user pipenv
+#python -m pip install --no-cache-dir --upgrade --user certifi
+
+for i in "${APP[@]}"
+do
+	cd "${i}"
+
+	echo
+	echo "Updating ${i} ..."
+	echo
+
+	pipenv --python 3.12 lock -v && pipenv --python 3.12 requirements --hash > requirements.txt
+	#pipenv lock && pipenv requirements > requirements.txt
+
+	git add Pipfile Pipfile.lock requirements.txt && \
+	git commit -s -m "Update requirements for ${i} ..." && \
+	git pull && \
+	git push
+
+	echo
+	echo
+
+	cd ..
+done
+
