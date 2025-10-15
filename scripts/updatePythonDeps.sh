01ce20cccd8854d4477c9e7aa22d0221d797eb10
Jauder Ho <jauderho@users.noreply.github.com>
2025-10-15 16:58:08 -0700

    Comment out installation of pipenv with python3.13
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

--- scripts/updatePythonDeps.sh
+++ scripts/updatePythonDeps.sh
@@ -26,7 +26,8 @@ git config --local pull.rebase false
 #PATH="$HOME/.local/bin:$PATH"
 #pipenv install --python 3.9
 #pipenv shell
-DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv python3.13
+#DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv python3.13
+DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends pipenv
 python3.13 -m pip install --no-cache-dir --upgrade pip --break-system-packages
 python3.13 -m pip install --no-cache-dir --upgrade --user pipenv certifi wheel setuptools packaging --break-system-packages
 #python3.11 -m pip install --no-cache-dir --upgrade --user pipenv
